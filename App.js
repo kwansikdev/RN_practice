d9c77da8e45e763b287da1298875ca724fc5cc85
kwansikdev <kwansikdev@gmail.com>
2022-02-10 00:49:27 +0900

    feat: 3.1 프로젝트 기반 다지기 (Finish) : Add Empty.js with Image

--- App.js
+++ App.js
@@ -8,8 +8,10 @@
 import React from 'react';
 import {StyleSheet} from 'react-native';
 import {SafeAreaProvider, SafeAreaView} from 'react-native-safe-area-context';
+import AddTodo from './components/AddTodo';
 
 import DateHead from './components/DateHead';
+import Empty from './components/Empty';
 
 const App = () => {
   const today = new Date();
@@ -17,13 +19,20 @@ const App = () => {
   console.log(today);
   return (
     <SafeAreaProvider>
-      <SafeAreaView edges={['bottom']}>
+      <SafeAreaView edges={['bottom']} style={styles.block}>
         <DateHead date={today} />
+        <Empty />
+        <AddTodo />
       </SafeAreaView>
     </SafeAreaProvider>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  block: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+});
 
 export default App;
