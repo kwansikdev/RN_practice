dcc11a1f12dde62ef8a00b58373a1282a7b23ad2
kwansikdev <kwansikdev@gmail.com>
2022-02-13 23:32:13 +0900

    4.4 할일 완료 상태 토글하기

--- App.js
+++ App.js
@@ -40,6 +40,14 @@ const App = () => {
     setTodos(todos.concat(todo));
   };
 
+  const onToggle = id => {
+    const nextTodos = todos.map(todo =>
+      todo.id === id ? {...todo, done: !todo.done} : todo,
+    );
+
+    setTodos(nextTodos);
+  };
+
   return (
     <SafeAreaProvider>
       <SafeAreaView edges={['bottom']} style={styles.block}>
@@ -47,7 +55,11 @@ const App = () => {
           behavior={Platform.select({ios: 'padding'})}
           style={styles.block}>
           <DateHead date={today} />
-          {todos.length === 0 ? <Empty /> : <TodoList todos={todos} />}
+          {todos.length === 0 ? (
+            <Empty />
+          ) : (
+            <TodoList todos={todos} onToggle={onToggle} />
+          )}
           <AddTodo onInsert={onInsert} />
         </KeyboardAvoidingView>
       </SafeAreaView>
