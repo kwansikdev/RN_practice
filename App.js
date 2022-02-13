f6c6fa33eb8b833944f1e5e3bda53911b3cd7792
kwansikdev <kwansikdev@gmail.com>
2022-02-13 23:21:03 +0900

    4.2 todos 상태 만들기 및 FlatList로 항목 화면 나타내기

--- App.js
+++ App.js
@@ -5,16 +5,23 @@
  * @format
  */
 
-import React from 'react';
+import React, {useState} from 'react';
 import {KeyboardAvoidingView, StyleSheet, Platform} from 'react-native';
 import {SafeAreaProvider, SafeAreaView} from 'react-native-safe-area-context';
 import AddTodo from './components/AddTodo';
 
 import DateHead from './components/DateHead';
 import Empty from './components/Empty';
+import TodoList from './components/TodoList';
 
 const App = () => {
   const today = new Date();
+
+  const [todos, setTosos] = useState([
+    {id: 1, text: '작업환경 설정', done: true},
+    {id: 2, text: '리액트 네이티브 기초 공부', done: false},
+    {id: 3, text: '투두리스트 만들어보기', done: false},
+  ]);
 
   return (
     <SafeAreaProvider>
@@ -23,7 +30,7 @@ const App = () => {
           behavior={Platform.select({ios: 'padding'})}
           style={styles.block}>
           <DateHead date={today} />
-          <Empty />
+          {todos.length === 0 ? <Empty /> : <TodoList todos={todos} />}
           <AddTodo />
         </KeyboardAvoidingView>
       </SafeAreaView>
