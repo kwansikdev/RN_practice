7f7f74324955dfb6530d36da2112efe2dd4fbecf
kwansikdev <kwansikdev@gmail.com>
2022-02-14 00:18:47 +0900

    init settings

---
+++ App.js
@@ -0,0 +1,8 @@
+import {NavigationContainer} from '@react-navigation/native';
+import React from 'react';
+
+const App = () => {
+  return <NavigationContainer>{/* 내비게이션 설정 */}</NavigationContainer>;
+};
+
+export default App;
