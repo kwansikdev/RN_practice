4fe55ea42c3594e3a5005756178835226a9b4118
kwansikdev <kwansikdev@gmail.com>
2022-02-15 00:43:17 +0900

    5.2 리액트 내비게이션 - 기본적인 사용법

--- App.js
+++ App.js
@@ -1,8 +1,70 @@
 import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import React from 'react';
+import {Text, TouchableOpacity, View} from 'react-native';
+import DetailScreen from './screens/DetailScreen';
+import HomeScreen from './screens/HomeScreen';
+import HeaderlessScreen from './screens/HeaderlessScreen';
+
+const Stack = createNativeStackNavigator();
 
 const App = () => {
-  return <NavigationContainer>{/* 내비게이션 설정 */}</NavigationContainer>;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Home"
+        screenOptions={{
+          headerShown: false,
+        }}>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{
+            title: '홈',
+            headerStyle: {
+              backgroundColor: '#29b6f6',
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+              fontWeight: 'bold',
+              fontSize: 20,
+            },
+          }}
+          // Header 블록에 대한 스타일
+        />
+        <Stack.Screen
+          name="Detail"
+          component={DetailScreen}
+          options={({route}) => ({
+            title: `상세 정보 - ${route.params.id}`,
+            headerBackVisible: false,
+            headerLeft: ({onPress}) => (
+              <TouchableOpacity onPress={onPress}>
+                <Text>Left</Text>
+              </TouchableOpacity>
+            ),
+            headerTitle: ({children}) => (
+              <View>
+                <Text>{children}</Text>
+              </View>
+            ),
+            headerRight: () => (
+              <View>
+                <Text>Right</Text>
+              </View>
+            ),
+          })}
+        />
+        <Stack.Screen
+          name="Headerless"
+          component={HeaderlessScreen}
+          options={{
+            headerShown: false,
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 };
 
 export default App;
