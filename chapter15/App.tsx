5967d8c596cb85c348b4e8216bccfe42f5b1dc38
kwansikdev <kwansikdev@gmail.com>
2022-02-22 00:12:43 +0900

    15.3 API 요청할 준비하기

--- chapter15/App.tsx
+++ chapter15/App.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import RootStack from './screens/RootStack';
+import {QueryClient, QueryClientProvider} from 'react-query';
+
+const queryList = new QueryClient();
 
 function App() {
   return (
-    <NavigationContainer>
-      <RootStack />
-    </NavigationContainer>
+    <QueryClientProvider client={queryList}>
+      <NavigationContainer>
+        <RootStack />
+      </NavigationContainer>
+    </QueryClientProvider>
   );
 }
 
