88545da60806963b773b723479325b568f459f35
kwansikdev <kwansikdev@gmail.com>
2022-02-24 00:25:37 +0900

    15.4 게시글 목록 조회 기능 구현하기

---
+++ chapter15/api/articles.ts
@@ -0,0 +1,8 @@
+import client from './client';
+import {Article} from './types';
+
+export async function getArticles() {
+  const response = await client.get<Article[]>('/articles');
+
+  return response.data;
+}
